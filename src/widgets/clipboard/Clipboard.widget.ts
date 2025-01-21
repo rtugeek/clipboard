f8d6b7117b4496c8690d385d6e7a73128b4ea045
Neo Fu <rtugeek@gmail.com>
2025-01-21 11:51:51 +0800

    🌐 (Clipboard.widget.ts): add English localization for widget title and description to support internationalization

--- src/widgets/clipboard/Clipboard.widget.ts
+++ src/widgets/clipboard/Clipboard.widget.ts
@@ -1,10 +1,9 @@
 import { BackgroundWidget, WidgetKeyword } from '@widget-js/core'
 
-// 组件标题
 const ClipboardWidget = new BackgroundWidget({
   name: 'cn.widgetjs.widgets.clipboard.list',
-  title: { 'zh-CN': '剪切板' },
-  description: { 'zh-CN': '带搜索、收藏功能的剪切板' },
+  title: { 'zh-CN': '剪切板', 'en-US': 'Clipboard' },
+  description: { 'zh-CN': '带搜索、收藏功能的剪切板', 'en-US': 'Clipboard with search and collection functions' },
   keywords: [WidgetKeyword.RECOMMEND],
   categories: ['productivity'],
   lang: 'zh-CN',
