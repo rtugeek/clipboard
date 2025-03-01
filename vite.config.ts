7a0a1cda03afeb4f6ff3c81a4f7a406d590411e7
Neo Fu <rtugeek@gmail.com>
2025-03-01 10:43:48 +0800

    ✨ remove unused vue-jsx plugin from configuration and dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import widget from '@widget-js/vite-plugin-widget'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -14,7 +13,6 @@ export default defineConfig({
   plugins: [
     vue(),
     widget({ generateFullNamePackage: true }),
-    vueJsx(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
